5704621b42c1dde3aa5ea5c908693614617cbd7e
goiwebdev <goiwebdev@gmail.com>
2016-09-25 14:20:00 +0800

    add initial model for Settled and Unsettled

---
+++ RiskApplication/Models/SettledBet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RiskApplication.Models
+{
+    public class SettledBet
+    {
+        public int CustomerId { get; set; }
+        public int EventId { get; set; }
+
+        public int ParticipantId { get; set; }
+        public int Stake { get; set; }
+
+        public int Win { get; set; }
+    }
+}
---
+++ RiskApplication/Models/UnsettledBet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RiskApplication.Models
+{
+    public class UnsettledBet
+    {
+        public int CustomerId { get; set; }
+        public int EventId { get; set; }
+
+        public int ParticipantId { get; set; }
+
+        public int Stake { get; set; }
+
+        public int ToWin { get; set; }
+
+
+    }
+}
